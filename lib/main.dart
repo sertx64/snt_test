f197c1a94e74dee6fdbcbce382f5a688acda9a33
Suvorov <sertx.fallen@bk.ru>
2024-10-04 21:43:45 +0400

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:snt_test/toGooTab.dart';
+import 'Presentation/Authorization.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'СНТ',
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: const Center(child: Text('Надо что-то передать')),
-      ),
-      floatingActionButton: const FloatingActionButton(onPressed: toGoogleTab),
-      body: Container(
-          color: const Color(0xFFB6AD9B),
-          padding: const EdgeInsets.all(16),
-          child: const Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Имя'),
-              TextField(
-                onChanged: toName,
-                style: TextStyle(fontSize: 22),
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                  ),
-                ),
-              ),
-              Text('Номер'),
-              TextField(
-                onChanged: toNumber,
-                style: TextStyle(fontSize: 22),
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                  ),
-                ),
-              ),
-              Text('Показания'),
-              TextField(
-                onChanged: toDate,
-                style: TextStyle(fontSize: 22),
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                  ),
-                ),
-              ),
-            ],
-          )),
-    );
-  }
-}
